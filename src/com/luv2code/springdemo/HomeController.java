8b06eb45aae94ef99f28126cab00d323f8121d6a
Yogesh Jadhav <yogesh.2jadhav@gmail.com>
2018-06-05 12:44:52 +0530

    Added .classpath and .project file

--- src/com/luv2code/springdemo/HomeController.java
+++ src/com/luv2code/springdemo/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-	@RequestMapping("/ws/")
+	@RequestMapping("/")
 	public String ShowPage() {
 		return "main-menu";
 	}
